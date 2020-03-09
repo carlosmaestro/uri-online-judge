3084f1acd631aaa7e555e139b7050dbacc6457a7
Carlos Henrique <chmmaestro@gmail.com>
2020-03-09 16:51:47 -0300

    updte 1018

--- 1018/1018.py
+++ 1018/1018.py
@@ -1,46 +1,14 @@
-valor = int(input())
-resutado = str(valor)+'\n'
+# -*- coding: utf-8 -*-
 
-mod100 = valor % 100
-if(mod100 == valor):
-    resutado += '%d nota(s) de R$ 100,00\n' % 0
-else:
-    resutado += '%d nota(s) de R$ 100,00\n' % (valor / 100)
+default_notas = [
+    100, 50, 20, 10, 5, 2, 1
+]
 
-mod50 = mod100 % 50
-if(mod50 == mod100):
-    resutado += '%d nota(s) de R$ 50,00\n' % 0
-else:
-    resutado += '%d nota(s) de R$ 50,00\n' % (mod100 / 50)
+value = int(input())
+print(value)
+for nota in default_notas:
 
-mod20 = mod50 % 20
-if(mod20 == mod50):
-    resutado += '%d nota(s) de R$ 20,00\n' % 0
-else:
-    resutado += '%d nota(s) de R$ 20,00\n' % (mod50 / 20)
+    count = int(value / nota)
 
-mod10 = mod20 % 10
-if(mod10 == mod20):
-    resutado += '%d nota(s) de R$ 10,00\n' % 0
-else:
-    resutado += '%d nota(s) de R$ 10,00\n' % (mod20 / 10)
-
-mod5 = mod10 % 5
-if(mod5 == mod10):
-    resutado += '%d nota(s) de R$ 5,00\n' % 0
-else:
-    resutado += '%d nota(s) de R$ 5,00\n' % (mod10 / 5)
-
-mod2 = mod5 % 2
-if(mod2 == mod5):
-    resutado += '%d nota(s) de R$ 2,00\n' % 0
-else:
-    resutado += '%d nota(s) de R$ 2,00\n' % (mod5 / 2)
-
-mod1 = mod2 % 1
-if(mod1 == mod2):
-    resutado += '%d nota(s) de R$ 1,00' % 0
-else:
-    resutado += '%d nota(s) de R$ 1,00' % (mod2 / 1)
-
-print(resutado)
+    print("{} nota(s) de R$ {},00".format(count, nota))
+    value -= count * nota
